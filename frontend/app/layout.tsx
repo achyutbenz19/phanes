543ffeaa36d092f44e2667378fd3332ff9821c05
achyutbenz19 <achyut.benz@gmail.com>
2024-04-22 22:14:47 -0400

    frontend phanes header + font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,49 +1,7 @@
-"use client"
-import axios from 'axios';
-import { ChangeEvent, useEffect, useState } from 'react';
-import ReconnectingWebSocket from 'reconnecting-websocket';
+import React from "react";
 
-const IndexPage = () => {
-  const [message, setMessage] = useState('');
-  const [value, setValue] = useState('');
-  const [socket, setSocket] = useState<any>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/');
-        setMessage(response.data.message);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setMessage('Failed to fetch data');
-      }
-    };
-    fetchData();
-
-    const rws = new ReconnectingWebSocket('ws://localhost:8000/ws?client_id=client-id');
-    setSocket(rws);
-
-    return () => {
-      rws.close();
-    };
-  }, []);
-
-  const sendMessage = () => {
-    if (socket && socket.readyState === WebSocket.OPEN) {
-      socket.send(JSON.stringify({ event: 'prompt', prompt: value }));
-    } else {
-      console.error('Socket not connected');
-    }
-  };
-
-  return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <p>Message from backend: {message}</p>
-      <input className='border' value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick={sendMessage}>Send Message</button>
-    </div>
-  );
+const Main = () => {
+  return <div className="m-3">Main</div>;
 };
 
-export default IndexPage;
+export default Main;
