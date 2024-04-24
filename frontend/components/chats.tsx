b762847b754c73ae0a1e20bab7ed4a0a825c38e9
achyutbenz19 <achyut.benz@gmail.com>
2024-04-24 11:30:15 -0400

    generates ui

--- frontend/components/chats.tsx
+++ frontend/components/chats.tsx
@@ -1,19 +1,37 @@
 "use client";
 import { useSocket } from "@/hooks/use-socket-store";
 import { processLogs } from "@/lib/functions/process-logs";
+import { useState } from "react";
 
 const Chats = () => {
+  const [messages, setMessages] = useState<any>([]);
   const { socket } = useSocket();
 
   if (socket) {
     socket.onmessage = function (event) {
-      console.log(event.data);
-      const result = processLogs(event.data);
-      console.log(result);
+      const { html, thought } = processLogs(event.data);
+      setMessages((currentMessages: any) => [
+        ...currentMessages,
+        {
+          display: thought ? (
+            <div>{thought}</div>
+          ) : html ? (
+            <div>
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+            </div>
+          ) : null,
+        },
+      ]);
     };
   }
 
-  return <div>Chats</div>;
+  return (
+    <div>
+      {messages.map((message: any, index: number) => (
+        <div key={index}>{message.display}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Chats;
--- frontend/lib/functions/process-logs.ts
+++ frontend/lib/functions/process-logs.ts
@@ -1,8 +1,9 @@
 export function processLogs(log: string) {
   const { event, data } = JSON.parse(log);
+  console.log(data);
   let html;
   let thought;
   if (event === "ui") html = data.html;
   if (event === "thought") thought = data.thought;
-  return { event, data, html, thought };
+  return { html, thought };
 }
